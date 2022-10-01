6814d43f209db29db573c31f84e91a1223a86120
seungjjun <il1uso@naver.com>
2022-10-01 22:03:41 +0900

    react 앱 생성 및 배포를 위한 package.json 설정 - ci.yml 파일 생성

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render((
+  <p>
+    Hello, world!
+  </p>
+));
